dd1f6ba239bf786611b931474b5df9d4aa95d909
betahikaru <beta.hikaru2000+github@gmail.com>
2015-04-19 00:03:02 +0900

    Initial commit

---
+++ config.ru
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + '/lib/aws_rest_server.rb'
+require 'aws_rest_server/application'
+
+run AwsRestServer::Application
---
+++ spec/aws_rest_server/application_spec.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
+
+require 'aws_rest_server/application'
+
+describe AwsRestServer::Application do
+  include Rack::Test::Methods
+
+  def app
+    @app ||= AwsRestServer::Application
+  end
+
+  %w{
+    /
+    /aws
+    /aws/iam
+    /aws/iam/users
+    /aws/iam/users?test=1
+    }.each do |uri|
+    describe "Exists '#{uri}' page" do
+      it "return 200 OK" do
+        get uri
+        expect(last_response).to be_ok
+      end
+    end
+  end
+
+end
